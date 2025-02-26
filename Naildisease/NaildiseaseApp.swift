ba2f559d36689320d8a1b10218d2361a04de3959
RMJP Kumara <piumalkumara@live.com>
2025-02-26 13:11:35 +0530

    Naildiseasesample project init

---
+++ Naildisease/NaildiseaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaildiseaseApp.swift
+//  Naildisease
+//
+//  Created by Piumal Kumara on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct NaildiseaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
